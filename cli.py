6510818d743dfa47e8fe3401c822468340dd8584
K0alabear <K0alabear@users.noreply.github.com>
2021-08-20 07:20:22 +0200

    Update cli.py

--- cli.py
+++ cli.py
@@ -3,23 +3,26 @@ import sys
 
 sys.path.insert(1, 'Attribute-Inference')
 import attack
-sys.path.insert(1, 'ModelInversion')
 
-import model_inversion 
+sys.path.insert(1, 'ModelInversion')
+import model_inversion
 
 
 @click.group()
 def cli():
     pass
 
+
 @cli.group()
-def attribute_inference(): 
+def attribute_inference():
     pass
+
 
 @attribute_inference.command(help='Load trained target and attack model')
 def pretrained_dummy():
     click.echo('Performing Attribute Inference with trained target and attack model')
     attack.perform_pretrained_dummy()
+
 
 @attribute_inference.command(help='Train target and attack model')
 @click.option('-t', '--target_epochs', default=30, help='Number of training epochs for the target model')
@@ -28,23 +31,58 @@ def train_dummy(target_epochs, attack_epochs):
     click.echo('Performing Attribute Inference with training of target and attack model')
     attack.perform_train_dummy(target_epochs, attack_epochs)
 
+
 @attribute_inference.command(help='Supply own target model and train attack model')
 @click.option('-c', '--class_file', required=True, type=str, help='File that holds the target models nn.Module class')
-@click.option('-s', '--state_path', required=True, type=str, help='Path of the state dictionary')
-@click.option('-d', '--dimension', required=True, type=int, help='Flattend dimension of the layer used as attack modelinput ')
+@click.option('-s', '--state_path', required=True, type=str, help='Path of the state dictio nary')
+@click.option('-d', '--dimension', required=True, type=int,
+              help='Flattend dimension of the layer used as attack modelinput ')
 @click.option('-a', '--attack_epochs', default=50, type=int, help='Number of training epochs for the attack model')
 def supply_target(class_file, state_path, dimension, attack_epochs):
     click.echo('Performing Attribute Inference')
     attack.perform_supply_target(class_file, state_path, dimension, attack_epochs)
 
+
 @cli.group()
 def membership_inference():
     pass
+
 
 @cli.group()
 def model_inversion():
     pass
 
+
+@model_inversion.command(help='load trained target model and perform inversion')
+def pretrained_dummy():
+    click.echo('Performing model inversion with trained target model')
+    model_inversion.perform_pretrained_dummy()
+
+
+@model_inversion.command(help='train target model')
+@click.option('--iterations', default=30, help='Number of Iterations in attack')
+@click.option('--epochs', default=30, help='Number of epochs for the target model')
+@click.option('--loss_function', default="crossEntropy", type=click.Choice(['crossEntropy', 'softmax']),
+              help='which loss function to use crossEntropy or softmax')
+@click.option('--number_of_results', default=-1, type=int,
+              help='choose Tag, number between 1 and 40, which you want recovered or nothing to get all recovered')
+def train_dummy(iterations, epochs, loss_function, number_of_results):
+    click.echo('Performing model inversion with training of target model')
+    model_inversion.perform_train_dummy(iterations, epochs, loss_function, number_of_results)
+
+
+# Todo: tag? and all?
+@model_inversion.command(help='use trained external target model and perform model inversion')
+@click.option('--class_file', required=True, type=str, help='target model file')
+@click.option('--iterations', default=30, type=int, help='Number of Iterations in the attack')
+@click.option('--loss_function', default="crossEntropy", type=click.Choice(['crossEntropy', 'softmax']),
+              help='which loss function to use crossEntropy or softmax')
+@click.option('--number_of_results', default=-1, type=int,
+              help='choose Tag, number between 1 and 40, which you want recovered or nothing to get all recovered')
+def supply_target(class_file, iterations, loss_function, number_of_results):
+    click.echo('performing Attribute Inference')
+    model_inversion.perform_supply_target(class_file, iterations, loss_function, number_of_results)
+
+
 if __name__ == "__main__":
     cli()
-    
