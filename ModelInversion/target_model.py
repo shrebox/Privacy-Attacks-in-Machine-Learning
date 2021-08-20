eb4b24a195bc3ce213689af340857c2318a75a14
K0alabear <K0alabear@users.noreply.github.com>
2021-08-20 07:19:38 +0200

    Update target_model.py

--- ModelInversion/target_model.py
+++ ModelInversion/target_model.py
@@ -13,7 +13,9 @@ import torchvision.datasets as datasets
 SEED = 12
 torch.manual_seed(SEED)
 torch.cuda.manual_seed(SEED)
-#torch.backends.cudnn.deterministic = True
+
+
+# torch.backends.cudnn.deterministic = True
 
 
 class MLP(nn.Module):
@@ -24,7 +26,6 @@ class MLP(nn.Module):
         self.output_fc = nn.Linear(3000, output_dim)
 
     def forward(self, x):
-
         batch_size = x.shape[0]
         x = x.view(batch_size, -1)
         h = torch.sigmoid(self.input_fc(x))
@@ -32,50 +33,48 @@ class MLP(nn.Module):
 
         return output, h
 
+
 def calculate_accuracy(y_pred, y):
-    top_pred = y_pred.argmax(1, keepdim = True)
+    top_pred = y_pred.argmax(1, keepdim=True)
     correct = top_pred.eq(y.view_as(top_pred)).sum()
     acc = correct.float() / y.shape[0]
     return acc
 
+
 def train(mlp, iterator, optimizer, criterion, device):
-    
     epoch_loss = 0
     epoch_acc = 0
-    
+
     mlp.train()
-    
+
     for (x, y) in iterator:
-        
         x = x.to(device)
         y = y.to(device)
-        
+
         optimizer.zero_grad()
-                
+
         y_pred, _ = mlp(x)
         loss = criterion(y_pred, y)
         acc = calculate_accuracy(y_pred, y)
-        
+
         loss.backward()
-        
+
         optimizer.step()
-        
+
         epoch_loss += loss.item()
         epoch_acc += acc.item()
-        
+
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
+
 def evaluate(mlp, iterator, criterion, device):
-    
     epoch_loss = 0
     epoch_acc = 0
-    
-    mlp.eval()
-    
-    with torch.no_grad():
-        
-        for (x, y) in iterator:
 
+    mlp.eval()
+
+    with torch.no_grad():
+        for (x, y) in iterator:
             x = x.to(device)
             y = y.to(device)
 
@@ -85,19 +84,20 @@ def evaluate(mlp, iterator, criterion, device):
 
             epoch_loss += loss.item()
             epoch_acc += acc.item()
-        
+
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-if __name__ == '__main__':
+
+def train_target_model(epochs):
     # transfrom, wee need grayscale to convert the images to 1 channel
     transform = transforms.Compose([
-                    transforms.Grayscale(),
-                    transforms.ToTensor(),
-                    transforms.Normalize((0.5), (0.5))
-                                ])
+        transforms.Grayscale(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5), (0.5))
+    ])
 
     # load dataset
-    atnt_faces = datasets.ImageFolder('data_pgm', transform=transform)
+    atnt_faces = datasets.ImageFolder('ModelInversion/data_pgm', transform=transform)
 
     # split dataset: 3 images of every class as validation set
     i = [i for i in range(len(atnt_faces)) if i % 10 > 3]
@@ -107,12 +107,12 @@ if __name__ == '__main__':
     BATCH_SIZE = 64
     train_dataset = torch.utils.data.Subset(atnt_faces, i)
     train_data_loader = data.DataLoader(train_dataset,
-                                 shuffle=True,
-                                 batch_size=BATCH_SIZE)
+                                        shuffle=True,
+                                        batch_size=BATCH_SIZE)
 
     validation_dataset = torch.utils.data.Subset(atnt_faces, i_val)
     validation_data_loader = data.DataLoader(validation_dataset,
-                                 batch_size=BATCH_SIZE)   
+                                             batch_size=BATCH_SIZE)
 
     # define dimensions                        
     INPUT_DIM = 112 * 92
@@ -123,20 +123,20 @@ if __name__ == '__main__':
 
     # set device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using device: %s'%device)
+    print('Using device: %s' % device)
     mlp = mlp.to(device)
 
     # set criterion and optimizer
     criterion = nn.CrossEntropyLoss()
-    #criterion = criterion.to(device)
+    # criterion = criterion.to(device)
     optimizer = optim.Adam(mlp.parameters())
 
     # main loop
-    EPOCHS = 30
+
     best_valid_loss = float('inf')
-    
+
     print('---Target Model Training Started---')
-    for epoch in range(EPOCHS):
+    for epoch in range(epochs):
 
         train_loss, train_acc = train(mlp, train_data_loader, optimizer, criterion, device)
         valid_loss, valid_acc = evaluate(mlp, validation_data_loader, criterion, device)
@@ -145,8 +145,8 @@ if __name__ == '__main__':
             best_valid_loss = valid_loss
             torch.save(mlp, 'atnt-mlp-model.pt')
 
-        print(f'Epoch: {epoch+1:02}')
-        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
-        print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
-        
+        print(f'Epoch: {epoch + 1:02}')
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
+        print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
+
     print('---Target Model Training Done---')
